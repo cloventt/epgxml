b3d801e9e7043dd5f0ff456358b37ae85a485dd3
David Palmer <david.palmer@adscale.co.nz>
2023-03-01 16:59:31 +1300

    fix incorrect icon urls

--- yahoo_jp.py
+++ yahoo_jp.py
@@ -44,7 +44,7 @@ CHANNEL_IDS = {
     '0x7FE60x0430': 'cloventt-jp-tvtokyo',
 }
 
-ICON_URL_PREFIX = 'https://github.com/cloventt/epgxml/blob/main/icons/'
+ICON_URL_PREFIX = 'https://raw.githubusercontent.com/cloventt/epgxml/main/icons/'
 
 
 def format_date(ts: int):
